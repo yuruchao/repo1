4df601432d60a4df7d77a3e3181e1875fbba9059
yuruchao <770981001@qq.com>
2020-09-15 19:03:07 +0800

    博士后管理系统

--- src/main/java/com/province/postdoctor/application/DemoApplication.java
+++ src/main/java/com/province/postdoctor/application/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.province.postdoctor;
+package com.province.postdoctor.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
